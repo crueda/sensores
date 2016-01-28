2000975aca5f4c8b7ef41a81af09c164e93b5bc0
Carlos Rueda Morales <crueda@gmail.com>
2016-01-28 08:01:42 +0100

    Create sensores.php

---
+++ sensores.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+<script type="text/javascript"
+  src="http://dygraphs.com/1.1.1/dygraph-combined.js"></script>
+</head>
+<body>
+
+
+<h1>Temperatura: <?php
+$file = fopen("temperatura_actual.log", "r");
+while(!feof($file)) {
+echo fgets($file);
+}
+fclose($file);
+?>
+</h1>
+<div id="graphdiv1"
+  style="width:1300px; height:500px;"></div>
+<script type="text/javascript">
+  g3 = new Dygraph(
+    document.getElementById("graphdiv1"),
+    "temperatura.log",
+    {
+      rollPeriod: 7,
+      showRangeSelector: true,
+      rangeSelectorHeight: 30,
+      showRoller: true
+    }
+  );
+  
+</script>
+
+<h1>Humedad: <?php
+$file = fopen("humedad_actual.log", "r");
+while(!feof($file)) {
+echo fgets($file);
+}
+fclose($file);
+?>
+</h1>
+<div id="graphdiv2"
+  style="width:1300px; height:500px;"></div>
+<script type="text/javascript">
+  g3 = new Dygraph(
+    document.getElementById("graphdiv2"),
+    "humedad.log",
+    {
+      rollPeriod: 7,
+      showRangeSelector: true,
+      rangeSelectorHeight: 30,
+      showRoller: true
+    }
+  );
+  
+</script>
+
+<h1>Temperatura y humedad</h1>
+<div id="graphdiv22"
+  style="width:1300px; height:500px;"></div>
+<script type="text/javascript">
+  g3 = new Dygraph(
+    document.getElementById("graphdiv22"),
+    "temperaturayhumedad.log",
+    {
+      rollPeriod: 7,
+      showRangeSelector: true,
+      rangeSelectorHeight: 30,
+      showRoller: true
+    }
+  );
+  
+</script>
+
+
+<h1>CPU</h1>
+<div id="graphdiv4"
+  style="width:1300px; height:500px;"></div>
+<script type="text/javascript">
+  g3 = new Dygraph(
+    document.getElementById("graphdiv4"),
+    "tempcpu.log",
+    {
+      rollPeriod: 7,
+      showRangeSelector: true,
+      rangeSelectorHeight: 30,
+      showRoller: true
+    }
+  );
+  
+</script>
+
+
+
+
+</body>
+</html>
